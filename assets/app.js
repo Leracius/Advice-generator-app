e07bbe2351a6226c02ed71062694fb49f3f35d0b
Axel Quintana <lucasaxelqg15@gmail.com>
2023-01-30 02:03:58 -0300

    first commit

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+const box = document.getElementById("box")
+const container = document.getElementById("advice-num")
+const adviceContainer = document.getElementById("advice")
+const buttonAd = document.getElementById("button")
+
+const advice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+    renderAdvice(adviceContainer, `"${data.slip.advice}"`)
+    renderAdvice(container, `A D V I C E  #  ${data.slip.id}`)
+}
+
+const renderAdvice = (el, message) =>{
+    el.innerText = message
+}
+
+buttonAd.addEventListener("click",advice)
